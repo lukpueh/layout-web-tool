e45c666aabc9c7ebf4dc1d92967b1caea88eed01
Isha Dave <ishavdave@nyu.edu>
2020-07-09 19:49:08 +0530

    Added comments and a docstring.

--- before_after_filesystem_snapshot.py
+++ before_after_filesystem_snapshot.py
@@ -1,4 +1,7 @@
 def before_after_snapshot(before_dict, after_dict):
+  '''before_after_snapshot is a simple function that outputs which files were
+    unchanged, modified, added or removed after a step was carried out.'''
+
   unchanged_files = []
   modified_files = []
   added_files = []
@@ -6,6 +9,7 @@ def before_after_snapshot(before_dict, after_dict):
   for key in before_dict:
     if key in after_dict:
       if before_dict[key] == after_dict[key]:
+        # Matching the hashes to check if file was unchanged
         unchanged_files.append(key)
       else:
         modified_files.append(key)
@@ -13,13 +17,16 @@ def before_after_snapshot(before_dict, after_dict):
       removed_files.append(key)
   for key in after_dict:
     if key not in before_dict:
+      # Looking for new files
       added_files.append(key)
 
+  # Returning the snapshot of the new file system
   print('unchanged = ', str(unchanged_files))
   print('modified = ', str(modified_files))
   print('added = ', str(added_files))
   print('removed = ', str(removed_files))
 
+# Test code:
 before = {
 'one.tgz': '1234567890abcdef',
 'foo/two.tgz': '0000001111112222',
